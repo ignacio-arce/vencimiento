5c2c89a571eeca174c0cbff25f06c558e374dd02
acerNacho <32885281+acerNacho@users.noreply.github.com>
2019-02-05 19:25:47 -0300

    Update ArchivoDeObjetos.java

--- src/poo/ArchivoDeObjetos.java
+++ src/poo/ArchivoDeObjetos.java
@@ -1,3 +1,5 @@
+package vencimiento.poo;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,8 +12,16 @@ import java.io.Serializable;
 import java.util.*;
 import java.text.*;
  
+
+
 public class ArchivoDeObjetos {
-    public void guardarObjeto(List<Vencimiento> ven){
+	private List<Vencimiento> vencimiento = new ArrayList<>();
+
+	ArchivoDeObjetos() {
+
+	}
+
+    public static void guardarVencimiento(List<Vencimiento> ven){
          
         OutputStream ops = null;
         ObjectOutputStream objOps = null;
@@ -33,33 +43,10 @@ public class ArchivoDeObjetos {
         }
          
     }
-     /*
-    public static void mostrarObjeto(){
-         
-        InputStream fileIs = null;
-        ObjectInputStream objIs = null;
-        try {
-            fileIs = new FileInputStream("archivoVencimientos.txt");
-            objIs = new ObjectInputStream(fileIs);
-            Vencimiento ven = (Vencimiento) objIs.readObject();
-            System.out.println(ven);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(objIs != null) objIs.close();
-            } catch (Exception ex){
-                 
-            }
-        }
-         
-    }*/
+
+
     
-    public static List<Vencimiento> mostrarObjeto(){
+    public static List<Vencimiento> leerVencimiento(){
          
         InputStream fileIs = null;
         ObjectInputStream objIs = null;
@@ -87,17 +74,9 @@ public class ArchivoDeObjetos {
     }
 
     public static void main(String a[]){
-		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-
-		Date today = new Date();
-		Date todayWithZeroTime = null;
-		try {
-			todayWithZeroTime = formatter.parse(formatter.format(today));
-		} catch (Exception e) {
-
-		}
+		
 		ArchivoDeObjetos ado = new ArchivoDeObjetos();
-		List<Vencimiento> ven = new ArrayList<>();
+		
 		Vencimiento v1 = new Vencimiento(todayWithZeroTime,2200,true,"Seguro del auto");
 		Vencimiento v2 = new Vencimiento(todayWithZeroTime,2200,true,"Seguro del auto");
 		ven.add(v1);
