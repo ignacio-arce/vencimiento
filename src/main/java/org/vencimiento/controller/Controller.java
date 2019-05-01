a5693af660313d13e9fe41fc7c0cdba4d90d1132
acernacho <ignacio.a.arg@gmail.com>
2019-05-01 15:28:19 -0300

    Se añadieron cambios

--- src/main/java/org/vencimiento/controller/Controller.java
+++ src/main/java/org/vencimiento/controller/Controller.java
@@ -88,7 +88,7 @@ public class Controller {
                                 int fecha[] = vencimientoDao.toInteger(((String) o[1]).split("-"));
 
                                 Vencimiento vencimientoElegido = new Vencimiento(LocalDate.of(fecha[0], fecha[1], fecha[2]), (String) o[0], (String) o[2]);
-
+ 
                                 for (Vencimiento v : vencimientoDao.getListaVencimientos()) {
                                     if (vencimientoElegido.compareTo(v) == 0) {
                                         vencimientoDao.borrarVencimiento(v);
