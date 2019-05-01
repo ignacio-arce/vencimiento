1ce1edf5aff2d828cd48a96c994235260b7d435a
acerNacho <32885281+acerNacho@users.noreply.github.com>
2019-05-01 15:13:17 -0300

    Update View.java

--- src/main/java/org/vencimiento/ui/View.java
+++ src/main/java/org/vencimiento/ui/View.java
@@ -21,6 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
