7f6c12d9abc27dcf4ac910b81a7cb9f82b87b817
acernacho <ignacio.a.arg@gmail.com>
2019-05-09 18:44:22 -0300

    Agregada clases relativas a DAOManager

---
+++ src/main/java/org/vencimiento/dao/DAOManager.java
@@ -0,0 +1,5 @@
+package dao;
+
+public interface DAOManager {
+	public VencimientoDao getVencimientoDao();
+}
---
+++ src/main/java/org/vencimiento/dao/SQLiteDAOManager.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteDAOManager implements DAOManager {
+
+	private static final String DRIVER = "org.sqlite.JDBC";
+	private static final String DBURL = "jdbc:sqlite:data.db";
+	private Connection conn;
+	private VencimientoDao vencimientoDao;
+
+	public SQLiteDAOManager() {
+
+		try {
+			Class.forName(DRIVER);
+			conn = getDBConnection();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public VencimientoDao getVencimientoDao() {
+		if (this.vencimientoDao == null) {
+			this.vencimientoDao = new VencimientoDaoImpl(conn);
+		}
+		return this.vencimientoDao;
+	}
+
+	private static Connection getDBConnection() {
+		Connection dbConnection = null;
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbConnection = DriverManager.getConnection(DBURL);
+			return dbConnection;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return dbConnection;
+
+	}
+
+}
